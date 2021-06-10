987a5d4feb4ff2b0e9baecc159d0875b74f414f0
Michel Memeteau <mm@ekimia.fr>
2021-06-10 11:26:20 +0200

    typo

--- virtual10.sh
+++ virtual10.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-echo "bonjours voulez vous telecharger windows 10" 
+echo "bonjour voulez vous telecharger windows 10" 
 
 zenity --question \
---title "Vitual10" \
+--title "Virtual10" \
 --text "Voulez vous telecharger et lancer Virtual 10"
  
 if [ $? = 0 ]
 then
 	echo "OUI ! Telechargement  "
-    # wget http://ftp.free.fr/mirrors/ftp.ubuntu.com/releases/20.04.2.0/ubuntu-20.04.2.0-desktop-amd64.iso -O /home/$USER/Documents/image.iso
-	sleep 3
+	wget https://drive.ekimia.fr/s/yQs5Y5YBqwPRKbH/download -O /home/$USER/Documents/virtual10.ova
 
-    echo " Installation de VirtualBox "
-    sudo apt install virtualbox
+	echo " Installation de VirtualBox "
+	sudo apt install virtualbox
 
-   	echo "OUI ! import du fichier OVA dans VirtualBox   "
-    vboxmanage import /home/$USER/Documents/Virtual10.ova
+   	echo "import du fichier OVA dans VirtualBox   "
+    vboxmanage import /home/$USER/Documents/virtual10.ova
     
 else
 	echo "NON ! Sortie"
